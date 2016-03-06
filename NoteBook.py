37f27f24d1a17bb57b467c98ebb92f7d9ba1a943
picasacasa <picasacasa@sina.cn>
2016-03-06 21:05:32 +0800

    Update NoteBook.py

--- NoteBook.py
+++ NoteBook.py
@@ -1,4 +1,13 @@
 import numpy as np
 import pandas as pd
 import tushare as ts
+import seaborn as sns
+
+#利用TuShare获取数据
+g000538 = ts.get_hist_data('000538')
+g000538.index[0] = '2016-03-04' #srting
+
+#将格式的时间文本转换为时间或日期
+from dateutil.parser import parse 
+parse(g000538.index[0]) = datetime.datetime(2016, 3, 4, 0, 0)
 
