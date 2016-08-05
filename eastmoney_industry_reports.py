da39f19be3517bc0b8df193b96c5713721099fa2
picasacasa <picasacasa@sina.cn>
2016-08-05 23:02:14 +0800

    Update eastmoney_industry_reports.py

--- eastmoney_industry_reports.py
+++ eastmoney_industry_reports.py
@@ -3,28 +3,22 @@ from bs4 import BeautifulSoup
 import requests
 import urllib
 
-# 东方财富通页面地址后缀格式
-page_list = ['x', 'y', 'z', '0', '1', '2', '3', '4', '5','xMA==', 'xMQ==',]
+# 东方财富通页面地址后缀格式，可用range代替
+page_number = 10
 
-# 东方财富通页面地址前缀格式
-page_url = 'http://data.eastmoney.com/report/hyyb.html#dHA9MCZjZz0wJmR0PTImcGFnZT0'
-
-pages = []
-for i in page_list:
-    pages.append(page_url + i)
-pages
+# 东方财富通页面地址前缀格式, 感谢http://www.mamicode.com/info-detail-1325623.html
+page_url = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=HYSR&mkt=0&stat=0&cmd=2&code=&sc=&ps=50&p=''
 
 
-soup3 = []
-for i in range(0, len(pages)-1):
+soup2 = []
+for i in range(1, page_number+1):
     html_doc = ''
-    html_doc = urllib.request.urlopen(pages[i]).read()
-    print(type(html_doc), len(html_doc))
-    soup = BeautifulSoup(html_doc, "lxml")
-    soup1 = str(soup.find_all('script'))
-    soup2 = soup1.split(':["')[1].split('"]')[0].split('","')
-    for j in range(0, len(soup2)-1):
-        soup3.append(soup2[j].replace('&sbquo;', '，').replace('&quot;', '\"').split(','))
+    html_doc = urllib.request.urlopen(page_url + str(i)).read()
+    print('第' + str(i) + '页提取成功')
+    soup0 = BeautifulSoup(html_doc, "lxml")
+    soup1 = soup.text[3:][:-3].split('","')
+    for j in range(0, len(soup1)-1):
+        soup2.append(soup1[j].replace('&sbquo;', '，').replace('&quot;', '\"').split(','))
 
-dataf = pd.DataFrame(soup3,)
+dataf = pd.DataFrame(soup2,)
 dataf.to_csv('/home/wangshi/script/dfcft_hy')
