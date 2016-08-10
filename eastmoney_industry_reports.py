5c312fcea39bdf94a03cb00c0b79bed99042b71f
picasacasa <picasacasa@sina.cn>
2016-08-11 01:17:22 +0800

    增加 文件处理部分

--- eastmoney_industry_reports.py
+++ eastmoney_industry_reports.py
@@ -10,23 +10,8 @@ page_number = 10
 # 东方财富通页面地址前缀格式, 感谢http://www.mamicode.com/info-detail-1325623.html
 page_url = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=HYSR&mkt=0&stat=0&cmd=2&code=&sc=&ps=50&p=''
 
-
-soup2 = []
-for i in range(1, page_number+1):
-    html_doc = ''
-    html_doc = urllib.request.urlopen(page_url + str(i)).read()
-    print('第' + str(i) + '页提取成功')
-    soup0 = BeautifulSoup(html_doc, "lxml")
-    soup1 = soup.text[3:][:-3].split('","')
-    for j in range(0, len(soup1)-1):
-        soup2.append(soup1[j].replace('&sbquo;', '，').replace('&quot;', '\"').split(','))
-
-dataf = pd.DataFrame(soup2,)
-dataf.to_csv('/home/wangshi/script/dfcft_hy')
-
 def get_eastmoney_industry_reports(pages):
     # 取回东方财富通行业报告函数
-    
     from bs4 import BeautifulSoup
     import requests
     import urllib
@@ -61,4 +46,8 @@ def get_eastmoney_report(urls):
     print('Download ' + str(len(urls)) + ' files '+ 'Successfully !')
     return
 
-    
+
+dataf = pd.DataFrame(soup3, columns = ['评级变动', '报告日期', '编号', '机构代码', '机构名称', '机构影响力', '行业代码', '评级类别', '投资评级', '标题', '行业名称', '涨跌幅'])
+dataf.index = dataf['编号']
+dataf.index.name = 'indexs'
+dataf.to_csv('/home/wangshi/script/dfcft_hy')
