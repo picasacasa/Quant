bb471118f18b8392226d453c0496a14c9ea6f70f
picasacasa <picasacasa@sina.cn>
2016-08-13 01:29:15 +0800

    更改 count_number 位置

--- eastmoney_industry_reports.py
+++ eastmoney_industry_reports.py
@@ -45,13 +45,14 @@ def get_eastmoney_industry_reports(pages):
 def get_eastmoney_report(urls, paths):
     from dateutil.parser import parse
     # 此处待添加验证 urls 是否为 DataFrame
+    count_number = 0 # 在 for 之前
     for i in range(0, len(urls)):
         temp_url = 'http://data.eastmoney.com/report/' + parse(urls.loc[urls.index[i]]['报告日期']).strftime('%Y'+'%m'+'%d') + '/hy,' + urls.loc[urls.index[i]]['编号'] + '.html'
         html_doc = urllib.request.urlopen(temp_url).read()
         soup = BeautifulSoup(html_doc, "lxml")
         file_url = soup.find_all(text = '查看PDF原文')[0].parent.get('href')
         temp_name = paths + parse(urls.loc[urls.index[i]]['报告日期']).strftime('%Y'+'%m'+'%d') + '_' + urls.loc[urls.index[i]]['机构名称'] + '_' + urls.loc[urls.index[i]]['标题'] + '.pdf'
-        count_number = 0
+        
         if os.path.isfile(temp_name) == True:
             pass
         else:
