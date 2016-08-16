928353ab5efae1b59c612fe97b9201fb33e1357d
picasacasa <picasacasa@sina.cn>
2016-08-17 01:08:30 +0800

    1.try处理错误 2.耗时计算

--- eastmoney_industry_reports.py
+++ eastmoney_industry_reports.py
@@ -1,11 +1,15 @@
 # coding: utf-8
+from datetime import datetime #导入日期时间
+start_time = datetime.now()
+
+print('+--------------------+') # 日志分隔符无意义
+
 from bs4 import BeautifulSoup
 import requests
 import urllib
 import pandas as pd
 import os #文件系统
 import shutil #文件复制
-from datetime import datetime
 
 # 东方财富通页面地址后缀格式，可用range代替
 page_number = 10
@@ -19,25 +23,28 @@ def get_eastmoney_industry_reports(pages):
     import requests
     import urllib
     import pandas as pd
+    try: #添加错误处理部分，待完善
+        soup2 = []
+        for i in range(1, pages+1):
+            html_doc = urllib.request.urlopen(page_url + str(i)).read()
+            # print('Get the ' + str(i) + 'th Page !')
+            soup0 = BeautifulSoup(html_doc, "lxml")
+            soup1 = soup0.text[3:][:-3].split('","')
+            for j in range(0, len(soup1)): #此处不能用len(soup1)-1,会缺少数据
+                soup2.append(soup1[j].replace('&sbquo;', '，').replace('&quot;', '\"').split(','))
     
-    soup2 = []
-    for i in range(1, pages+1):
-        html_doc = urllib.request.urlopen(page_url + str(i)).read()
-        print('第' + str(i) + '页提取成功')
-        soup0 = BeautifulSoup(html_doc, "lxml")
-        soup1 = soup0.text[3:][:-3].split('","')
-        for j in range(0, len(soup1)): #此处不能用len(soup1)-1,会缺少数据
-            soup2.append(soup1[j].replace('&sbquo;', '，').replace('&quot;', '\"').split(','))
+        dataf = pd.DataFrame(soup2, columns = ['评级变动', '报告日期', '编号', '机构代码', '机构名称', '机构影响力', '行业代码', '评级类别', '投资评级', '标题', '行业名称', '涨跌幅'])
+        dataf.index = dataf['编号']
+        dataf.index.name = 'indexs'
     
-    dataf = pd.DataFrame(soup2, columns = ['评级变动', '报告日期', '编号', '机构代码', '机构名称', '机构影响力', '行业代码', '评级类别', '投资评级', '标题', '行业名称', '涨跌幅'])
-    dataf.index = dataf['编号']
-    dataf.index.name = 'indexs'
-    
-    # 去掉标题中的半角引号、半角冒号
-    for i in range(0, len(dataf)-1):
-        dataf['标题'][i] = dataf['标题'][i].replace(':', '：').replace('"', '')
-    # dataf.to_csv('/home/wangshi/script/dfcft_hy')
-    # 返回DataFrame
+        # 去掉标题中的半角引号、半角冒号
+        for i in range(0, len(dataf)-1):
+            dataf['标题'][i] = dataf['标题'][i].replace(':', '：').replace('"', '')
+        # dataf.to_csv('/home/wangshi/script/dfcft_hy')
+        # 返回DataFrame
+    except:
+        print('Fail to get the data!')
+    print('Get ' + str(pages) + ' pages!')
     return dataf
 
 
@@ -45,20 +52,33 @@ def get_eastmoney_industry_reports(pages):
 def get_eastmoney_report(urls, paths):
     from dateutil.parser import parse
     # 此处待添加验证 urls 是否为 DataFrame
-    count_number = 0 # 在 for 之前
+    count_download = 0
+    count_pass = 0
+    count_fail = 0
     for i in range(0, len(urls)):
         temp_url = 'http://data.eastmoney.com/report/' + parse(urls.loc[urls.index[i]]['报告日期']).strftime('%Y'+'%m'+'%d') + '/hy,' + urls.loc[urls.index[i]]['编号'] + '.html'
+        # print(temp_url)
         html_doc = urllib.request.urlopen(temp_url).read()
         soup = BeautifulSoup(html_doc, "lxml")
-        file_url = soup.find_all(text = '查看PDF原文')[0].parent.get('href')
-        temp_name = paths + parse(urls.loc[urls.index[i]]['报告日期']).strftime('%Y'+'%m'+'%d') + '_' + urls.loc[urls.index[i]]['机构名称'] + '_' + urls.loc[urls.index[i]]['标题'] + '.pdf'
+        try: #增加错误处理部分，可再优化
+            file_url = soup.find_all(text = '查看PDF原文')[0].parent.get('href')
+            temp_name = paths + parse(urls.loc[urls.index[i]]['报告日期']).strftime('%Y'+'%m'+'%d') + '_' + urls.loc[urls.index[i]]['机构名称'] + '_' + urls.loc[urls.index[i]]['标题'] + '.pdf'
         
-        if os.path.isfile(temp_name) == True:
+            if os.path.isfile(temp_name) == True:
+                # print('File already exist! PASS!')
+                count_pass += 1
+                pass
+            else:
+                urllib.request.urlretrieve(file_url, temp_name)
+                count_download += 1
+                # print('Download ' + str(count_download) + '/' + str(len(urls)) + ' new files '+ 'Successfully !')
+        except:
+            print('Fail to get the file. ' + 'Please download the file manually !' + '\n'  + temp_url)
+            count_fail += 1
             pass
-        else:
-            urllib.request.urlretrieve(file_url, temp_name)
-            count_number += 1
-    print('Download ' + str(count_number) + '/' + str(len(urls)) + ' new files '+ 'Successfully !')
+        
+    print('    DOWN: ' + str(count_download) + '/' + str(len(urls)) + '\n'  + '    PASS: ' + str(count_pass) + '/' + str(len(urls)) + '\n'   + '    FAIL: ' + str(count_fail) + '/' + str(len(urls)))
+
     return
 
 # DataFrame 的差
@@ -104,6 +124,9 @@ get_eastmoney_report(food_beverage, '/home/wangshi/reports/food_beverage/')
 
 
 # 将新的文件存盘，并备份为含日期格式的文件
-# 将新的文件存盘，并备份为含日期格式的文件
 data_all.to_csv('/home/wangshi/reports/script/dfcft_hy')
 shutil.copyfile('/home/wangshi/reports/script/dfcft_hy', ('/home/wangshi/reports/script/dfcft_hy_' + str((datetime.now()).strftime('%Y%m%d'))))
+
+# 输出处理时间到日志
+end_time = datetime.now()
+print('USED ' + str((end_time - start_time).seconds) + ' seconds!')
