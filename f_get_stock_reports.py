437be25a7d96f1485b079476fca5a9bd7104642a
picasacasa <picasacasa@sina.cn>
2016-09-01 01:16:00 +0800

    完善异常处理

--- f_get_stock_reports.py
+++ f_get_stock_reports.py
@@ -12,6 +12,9 @@ from bs4 import BeautifulSoup
 import requests, urllib, os, shutil, json
 import pandas as pd
 
+# 导入异常记录模块
+import traceback 
+
 def get_sw_industry_list(industry_code): # 不用 list ，用 text 输入
     
     # 引入所需包
@@ -100,11 +103,12 @@ def get_eastmoney_stock_report(urls, paths, code, name):
     count_pass = 0
     count_fail = 0
     for i in range(0, len(urls)):
-        temp_url_3 = 'http://data.eastmoney.com/report/' + parse(urls.loc[urls.index[i]]['datetime']).strftime('%Y'+'%m'+'%d') + '/' + urls.loc[urls.index[i]]['infoCode'] + '.html'
-        # print(temp_url_3)
-        html_doc_3 = urllib.request.urlopen(temp_url_3).read()
-        soup_3 = BeautifulSoup(html_doc_3, "lxml")
         try:
+            temp_url_3 = 'http://data.eastmoney.com/report/' + parse(urls.loc[urls.index[i]]['datetime']).strftime('%Y'+'%m'+'%d') + '/' + urls.loc[urls.index[i]]['infoCode'] + '.html'
+            # print(temp_url_3)
+            html_doc_3 = urllib.request.urlopen(temp_url_3).read()
+            soup_3 = BeautifulSoup(html_doc_3, "lxml")
+
             file_url_3 = soup_3.find_all(text = '查看PDF原文')[0].parent.get('href')
             temp_name_3 = paths + parse(urls.loc[urls.index[i]]['datetime']).strftime('%g'+'%m'+'%d') + '_' + code + '_' + name + '_' + urls.loc[urls.index[i]]['insName'] + '_' + urls.loc[urls.index[i]]['title'] + '.pdf'
         
@@ -117,12 +121,13 @@ def get_eastmoney_stock_report(urls, paths, code, name):
                 count_download += 1
                 # print('Download ' + str(count_download) + '/' + str(len(urls)) + ' new files '+ 'Successfully !')
         except:
-            print('Fail to get the file. ' + 'Please download the file manually !' + '\n'  + temp_url_3)
+            print('Fail to get the file of ' + code + '_' + name + ',' + 'Please download the file manually !' + '\n'  + temp_url_3)
             count_fail += 1
             pass
         
     if count_download == 0 and count_pass == 0 and count_fail == 0:
         pass
+    
     else:
         print(code + '_' + name + '：' + 'DOWN:' + str(count_download) + '_' + 'PASS:' + str(count_pass) + '_' + 'FAIL:' + str(count_fail))
     
@@ -146,47 +151,53 @@ def get_stock_report_list(DataFrame, path = '/home/wangshi/script/stocks_reports
             url_data_0 = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20PrnJnSby={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=25&p=1&code=' + stock_code
             html_doc_0 = urllib.request.urlopen(url_data_0).read()
             soup_0 = BeautifulSoup(html_doc_0, "lxml")
-        
-            # 用 json 处理得到的 json 文本
-            jsontext_0 = json.loads(soup_0.text.split('=')[1])
-        
-            # 向上取整取得报告页数
-            page_numbers = ceil(int(jsontext_0['count'])/25)
-        
-            data_0 = []
-        
-            # 第二层循环，遍历股票报告页面，取得其报告编码
-            for page_number in range(1, page_numbers + 1):
-            
-                url_data_1 = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20PrnJnSby={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=25&p=' + str(page_number) + '&code=' + stock_code
-                html_doc_1 = urllib.request.urlopen(url_data_1).read()
-                soup_1 = BeautifulSoup(html_doc_1, "lxml")
-            
-                # 用 json 处理得到的 json 文本
-                jsontext_1 = json.loads(soup_1.text.split('=')[1])
-            
-                #合并新的列表
-                data_0 += jsontext_1['data']
-        
-            #处理取得的编码为DataFrame，并将结果存盘
-            data_1 = pd.DataFrame(data_0)
-            data_1.index = data_1['infoCode']
-            data_1.index.name = 'infoCodes' # 为避免名称重复这里设置为 infoCodes
-            
-            # 识别旧数据是否存在
-            if os.path.isfile(path + stock_code) == True:
-                data_old = pd.read_csv(path + stock_code, index_col = 'infoCodes')
-                data_delta = df_diff(data_1, data_old)
 
+            # 用 json 处理得到的 json 文本
+            jsontext_0 = json.loads(soup_0.text.split('=', 1)[1].replace('[{stats:false}]','"[{stats:false}]"'))
+            
+            # 向上取整取得报告页数
+            if jsontext_0['data'] == '[{stats:false}]':
+                print(stock_code + '_' + DataFrame['证券名称'].loc[stock_code] + ' Reports Not Exist')
+                pass
+                
             else:
-                data_delta = data_1
+                page_numbers = ceil(int(jsontext_0['count'])/25)
+        
+                data_0 = []
+        
+                # 第二层循环，遍历股票报告页面，取得其报告编码
+                for page_number in range(1, page_numbers + 1):
+            
+                    url_data_1 = 'http://datainterface.eastmoney.com//EM_DataCenter/js.aspx?type=SR&sty=GGSR&js=var%20PrnJnSby={%22data%22:[(x)],%22pages%22:%22(pc)%22,%22update%22:%22(ud)%22,%22count%22:%22(count)%22}&ps=25&p=' + str(page_number) + '&code=' + stock_code
+                    html_doc_1 = urllib.request.urlopen(url_data_1).read()
+                    soup_1 = BeautifulSoup(html_doc_1, "lxml")
+            
+                    # 用 json 处理得到的 json 文本
+                    jsontext_1 = json.loads(soup_1.text.split('=', 1)[1].replace('[{stats:false}]','"[{stats:false}]"'))
+            
+                    #合并新的列表
+                    data_0 += jsontext_1['data']
+        
+                #处理取得的编码为DataFrame，并将结果存盘
+                data_1 = pd.DataFrame(data_0)
+                data_1.index = data_1['infoCode']
+                data_1.index.name = 'infoCodes' # 为避免名称重复这里设置为 infoCodes
+            
+                # 识别旧数据是否存在
+                if os.path.isfile(path + stock_code) == True:
+                    data_old = pd.read_csv(path + stock_code, index_col = 'infoCodes')
+                    data_delta = df_diff(data_1, data_old)
+    
+                else:
+                    data_delta = data_1
                
             
-            get_eastmoney_stock_report(data_delta, DataFrame['文件夹'].loc[stock_code], DataFrame['证券代码'].loc[stock_code], DataFrame['证券名称'].loc[stock_code])
+                    get_eastmoney_stock_report(data_delta, DataFrame['文件夹'].loc[stock_code], DataFrame['证券代码'].loc[stock_code], DataFrame['证券名称'].loc[stock_code])
             
-            data_1.to_csv(path + stock_code)
+                    data_1.to_csv(path + stock_code)
         
         except:
+            traceback.print_exc()
             print('Fail to get reports of ' + DataFrame['证券代码'].loc[stock_code] + '_' + DataFrame['证券名称'].loc[stock_code])
 
     return
