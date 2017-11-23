1ea555c37832c7f066cd32b49fb1ab2ee2dbb9c1
picasacasa <picasacasa@sina.cn>
2017-11-23 23:16:35 +0800

    fix bug

--- def.py
+++ def.py
@@ -1,11 +1,13 @@
 # DataFrame 的差
 def df_diff(df_a, df_b):
+    import pandas as pd
     df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
     return df_a_b
 
 
 # DataFrame 的并
 def df_all(df_a, df_b):
+    import pandas as pd
     df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
     df_all = df_a_b.append(df_b)
     return df_all
