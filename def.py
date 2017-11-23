8008cd99d9c66828063de2c06ee417d91d1e7b7d
picasacasa <picasacasa@sina.cn>
2017-11-23 23:15:18 +0800

    get_soup,get_web_json

--- def.py
+++ def.py
@@ -9,3 +9,21 @@ def df_all(df_a, df_b):
     df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
     df_all = df_a_b.append(df_b)
     return df_all
+
+# 获取网页内容
+def get_soup(url):
+    from bs4 import BeautifulSoup
+    import requests, urllib
+    html_doc = urllib.request.urlopen(temp_url).read()
+    soup = BeautifulSoup(html_doc, "lxml")
+    return soup
+
+# 获取网页json内容
+def get_web_json(url,decodes = 'utf-8'):
+    import requests, urllib
+    html_list = urllib.request.urlopen(url_list).read()
+    jsontext = json.loads(html_list.decode('utf-8'))
+    return jsontext
+    
+
+
