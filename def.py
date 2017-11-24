33616f9d68fa8df5e5ce8801345104d2f40b9cf6
picasacasa <picasacasa@sina.cn>
2017-11-25 00:56:57 +0800

    add chinese_able_url

--- def.py
+++ def.py
@@ -1,16 +1,16 @@
-# DataFrame 的差
-def df_diff(df_a, df_b):
-    import pandas as pd
-    df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
-    return df_a_b
+	# DataFrame 的差
+	def df_diff(df_a, df_b):
+	    import pandas as pd
+	    df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
+	    return df_a_b
 
 
-# DataFrame 的并
-def df_all(df_a, df_b):
-    import pandas as pd
-    df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
-    df_all = df_a_b.append(df_b)
-    return df_all
+	# DataFrame 的并
+	def df_all(df_a, df_b):
+	    import pandas as pd
+	    df_a_b = df_a.ix[df_a.index.difference(df_b.index)]
+	    df_all = df_a_b.append(df_b)
+	    return df_all
 
 # 获取网页内容
 def get_soup(url):
@@ -28,4 +28,10 @@ def get_web_json(url,decodes = 'utf-8'):
     return jsontext
     
 
-
+# 用于解决URL编码中带有中文错误的问题
+# 类似 'ascii' codec can't encode characters in position 66-70: ordinal not in range(128)
+def chinese_able_url(url):
+    from urllib.parse import quote
+    import  string
+    url = quote(url, safe = string.printable)
+    return url
